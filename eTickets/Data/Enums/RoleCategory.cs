f2b4b3e03de0e0453ebc2e96e79591acf3937db9
unknown <mahmoudahmed10197@gmail.com>
2023-05-08 23:10:36 +0300

    Upload

---
+++ eTickets/Data/Enums/RoleCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data
+{
+    public enum RoleCategory
+    {
+        [Description("Admin")]
+        Admin = 1,
+        [Description("User")]
+        User
+    }
+}
